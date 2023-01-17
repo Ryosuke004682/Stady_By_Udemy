6c4f7e7c8572b63e73b349060f6e62e93408770e
ryousuke <83821881+Ryosuke004682@users.noreply.github.com>
2023-01-18 02:12:52 +0900

    キャスト、文字列変換、定数

---
+++ Udemy_C#/Constant_C/Constant_C/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constant_C
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            const int NUMBER = 100;
+            const string STRING = "Hoge";
+
+            Console.WriteLine(NUMBER);
+            Console.WriteLine(STRING);
+
+            //constがついた変数は変えられない。
+            //NUMBER = 10;
+            //STRING = "piyo";
+
+        }
+    }
+}
