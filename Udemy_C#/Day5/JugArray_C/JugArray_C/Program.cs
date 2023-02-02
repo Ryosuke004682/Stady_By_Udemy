676c6fc00ae809af1bebffe486f8ef21a8b9b918
ryousuke <83821881+Ryosuke004682@users.noreply.github.com>
2023-02-02 16:17:15 +0900

    ジャグ配列と多重配列

---
+++ Udemy_C#/Day5/JugArray_C/JugArray_C/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JugArray_C
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[][] a = new int[][]
+            {
+                new int[] {0,1 },
+                new int[] {2,3,4},
+                new int[] {5,6,7,8,9}
+            };
+
+            for(var i = 0; i < a.Length; i++)
+            {
+                for(var j = 0; j < a[i].Length; j++)
+                {
+                    Console.Write(a[i][j] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ Udemy_C#/Day5/MultidimensionalArray_C/MultidimensionalArray_C/MultidimensionalArray_C/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultidimensionalArray_C
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] a = new int[3,4];
+
+            for(var i = 0; i < 3; i++)
+            {
+                for(var j = 0; j < 4; j++)
+                {
+                    a[i, j] = i + j;
+                }
+            }
+
+            for(var i = 0; i < 3; i++)
+            {
+                for(var j = 0; j < 4; j++)
+                {
+                    Console.Write($" a[{i},{j}]={a[i,j]}");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
